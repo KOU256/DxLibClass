a388497cc1c87a5ce078e64992389480480e77e5
Sugawara <g15219@g.ichinoseki.ac.jp>
2018-06-30 17:49:50 +0900

    Update object.hpp
    Objectクラスのpublic変数を宣言。

--- header/object.hpp
+++ header/object.hpp
@@ -1,4 +1,14 @@
-#ifndef	INCLUDE_GUARD_OBJECT_HPP
-#define	INCLUDE_GUARD_OBJECT_HPP
+#ifndef INCLUDE_GUARD_OBJECT_HPP
+#define INCLUDE_GUARD_OBJECT_HPP
+
+class Object{
+private:
+	int m_x;
+	int m_y;
+	int m_grapchic_handle;
+	int m_grapchic_size_x;
+	int m_grapchic_size_y;
+  public:
+};
 
 #endif
